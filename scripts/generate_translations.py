b5632d31540f2e91a2c04bf399a0456eec6c7491
Loi Tran <primetimetran@gmail.com>
2023-07-16 18:09:47 -0400

    Update translation & add question script

--- scripts/generate_translations.py
+++ scripts/generate_translations.py
@@ -1,6 +1,7 @@
 from googletrans import Translator
 import json
 import codecs
+import os
 
 translator = Translator()
 
@@ -16,12 +17,20 @@ def translate_dict(data, dest_language):
             translated_dict[key] = translate_dict(value, dest_language)
         else:
             translated_dict[key] = translator.translate(value, src='en', dest=dest_language).text.title().title()
+            print(translated_dict[key])
     return translated_dict
 
+
+script_dir = os.path.dirname(__file__) 
+rel_path = "../lib/l10n/app_en.arb"
+abs_file_path = os.path.join(script_dir, rel_path)
+
 for l in languages:
-    with open('app_en.arb') as file:
+    with open(abs_file_path) as file:
         data = json.load(file)
         translated_data = translate_dict(data, l)
 
-        with codecs.open("app_" + l + ".arb", "w", encoding='utf8') as f:
+        with codecs.open(os.path.join(script_dir, '../lib/l10n/app_')  + l + ".arb", "w", encoding='utf8') as f:
             json.dump(translated_data, f, ensure_ascii=False)
+
+print('Successfully translated!')
